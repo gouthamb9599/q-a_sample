4bb45a1030a5392c80ccf62082e314eafa69d499
gouthamb9599 <gouthamrahul1095@gmail.com>
2020-06-26 02:03:42 +0530

    adding questions completed

--- quora-be/route/approute.js
+++ quora-be/route/approute.js
@@ -9,27 +9,27 @@ const route = app => {
             (err, results) => {
                 if (err) console.log(err);
                 else {
-                    console.log(results);
+                    // console.log(results);
                     if (results.rowCount === 1) {
                         let token = jwt.sign({ data: google, exp: Math.floor(Date.now() / 100) + 600 * 600 },
                             "secret"
                         );
-                        console.log(token)
-                        res.send({ success: true, token, Name: google.Name });
+                        // console.log(token)
+                        res.send({ success: true, token, Name: google.Name, id: results.rows[0].id });
                     }
                     else {
-                        client.query(`insert into details(name,email,providername,image,token,password) values($1,$2,$3,$4,$5,$6) RETURNING *`,
+                        client.query(`insert into details(name,email,providername,image,token,password) values($1,$2,$3,$4,$5,$6) RETURNING id`,
                             [google.Name, google.email, google.ProviderId, google.Image, google.token, password],
                             (err, results) => {
                                 if (err) console.log(err);
                                 else {
                                     console.log("user data entered successfully through google ID");
-                                    console.log(results)
+                                    // console.log(results)
                                     let token = jwt.sign({ data: google, exp: Math.floor(Date.now() / 100) + 600 * 600 },
                                         "secret"
                                     );
-                                    console.log(token)
-                                    res.send({ success: true, token, Name: google.Name });
+                                    // console.log(token)
+                                    res.send({ success: true, token, Name: google.Name, id: results.rows[0].id });
                                 }
 
                             });
@@ -45,11 +45,11 @@ const route = app => {
                 if (err) console.log(err);
                 else {
                     console.log('access successful')
-                    console.log(results.rows[0])
+                    // console.log(results.rows[0])
                     let token = jwt.sign({ data: results.rows[0], exp: Math.floor(Date.now() / 100) + 600 * 600 },
                         "secret")
-                    console.log(token);
-                    res.send({ success: true, token, Name: results.rows[0].name })
+                    // console.log(token);
+                    res.send({ success: true, token, Name: results.rows[0].name, id: results.rows[0].id })
                 }
 
             })
@@ -65,6 +65,29 @@ const route = app => {
                 }
             })
 
+    })
+    app.get("/gettags", (req, res) => {
+        client.query('select * from tags', (err, results) => {
+            if (err) console.log(err);
+            else {
+                console.log(results.rows);
+                res.send({ success: true, data: results.rows })
+            }
+        })
+    })
+    app.post('/addquestion', (req, res) => {
+        console.log(req.body);
+        const questiondata = req.body;
+        client.query(`insert into questions(question_heading,question_desc,user_id,tag_id) values($1,$2,$3,$4)`,
+            [questiondata.questionheading, questiondata.questiondesc, questiondata.userid, questiondata.tag],
+            (err, results) => {
+                if (err) console.log(err);
+                else {
+                    if (results.rowCount !== 0) {
+                        res.send({ success: true })
+                    }
+                }
+            })
     })
 };
 module.exports = route;
--- quora-fe/src/components/question.jsx
+++ quora-fe/src/components/question.jsx
@@ -1,28 +1,80 @@
-import React from 'react';
+import React, { useMemo, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import TextField from '@material-ui/core/TextField';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import FormControl from '@material-ui/core/FormControl';
 import { useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import Axios from 'axios';
+import { MenuItem } from '@material-ui/core';
+import swal from 'sweetalert';
 
 export default function Question() {
+    const [tagid, settagid] = React.useState(0);
     const [open, setOpen] = React.useState(true);
+    const [Taglist, setTaglist] = React.useState([]);
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    useEffect(() => {
+        Axios.get(`http://localhost:5000/gettags`)
+            .then(res => {
+                console.log(res.data);
+                setTaglist(res.data.data);
+                console.log(Taglist);
+            })
+    }, [])
+    const useStyles = makeStyles((theme) => ({
+        button: {
+            display: 'block',
+            marginTop: theme.spacing(2),
+        },
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120,
+        },
+    }));
+    const classes = useStyles();
+    const [opendrop, setOpendrop] = React.useState(false);
+    const [Questionheading, setQuestionheading] = React.useState("");
+    const [Questiondesc, setQuestiondesc] = React.useState("");
 
-    const handleClickOpen = () => {
-        setOpen(true);
+    const handleCloses = () => {
+        setOpendrop(false);
     };
 
+    const handleOpen = () => {
+        setOpendrop(true);
+    };
     const handleClose = () => {
         setOpen(false);
-    };
-    const getTags = () => {
+    }
+    const handlesubmit = () => {
 
+        const userData = JSON.parse(sessionStorage.getItem('userData'));
+        console.log(userData.data.id);
+
+        Axios.post(`http://localhost:5000/addquestion`, { questionheading: Questionheading, questiondesc: Questiondesc, tag: tagid, userid: userData.data.id })
+            .then(res => {
+                if (res.data.success === true) {
+                    swal("your Question is posted ", "check your homepage for more questions", "success")
+                }
+            })
+        setOpen(false);
+    };
+    const handleChangeid = (event) => {
+        settagid(event.target.value);
+    };
+    const handleChangehead = (event) => {
+        setQuestionheading(event.target.value)
+    }
+    const handleChangedesc = (event) => {
+        setQuestiondesc(event.target.value)
     }
 
     return (
@@ -38,22 +90,44 @@ export default function Question() {
                         Add questions to get clarified by others
           </DialogContentText>
 
-                    <textarea name="paragraph_text" cols="50" rows="2" placeholder="Question Title"></textarea>
-                    <textarea name="paragraph_text" cols="50" rows="5" placeholder="Question Description"></textarea>
-                    <TextField
+                    <textarea name="questionhead" cols="50" rows="2" onChange={handleChangehead} placeholder="Question Title"></textarea>
+                    <textarea name="questiondesc" cols="50" rows="5" onChange={handleChangedesc} placeholder="Question Description"></textarea>
+                    <Button className={classes.button} onClick={handleOpen}>
+                        Open the select
+      </Button>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-controlled-open-select-label">Tag</InputLabel>
+                        <Select
+                            labelId="demo-controlled-open-select-label"
+                            id="demo-controlled-open-select"
+                            open={opendrop}
+                            onClose={handleCloses}
+                            onOpen={handleOpen}
+                            value={tagid}
+                            onChange={handleChangeid}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            {Taglist.map((data) => (
+                                <MenuItem value={data.id}>{data.name}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    {/* <TextField
                         autoFocus
                         margin="dense"
                         id="name"
                         label=""
                         type="email"
                         fullWidth
-                    />
+                    /> */}
                 </DialogContent>
                 <DialogActions>
                     <Button autoFocus onClick={handleClose} color="primary">
                         Close
           </Button>
-                    <Button onClick={handleClose} color="primary" autoFocus>
+                    <Button onClick={handlesubmit} color="primary" autoFocus>
                         Submit
           </Button>
                 </DialogActions>
